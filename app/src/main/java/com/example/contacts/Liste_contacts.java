f973de3bc7cf2d0cb108429d4d6526ba5407dd73
Taoufik Rachad <rachad.taoufik@gmail.com>
2023-02-22 10:28:51 +0100

    Version4 ok

--- app/src/main/java/com/example/contacts/Liste_contacts.java
+++ app/src/main/java/com/example/contacts/Liste_contacts.java
@@ -1,23 +1,38 @@
 package com.example.contacts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.contacts.Contact;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
 
 public class Liste_contacts extends AppCompatActivity implements View.OnClickListener {
 
     FloatingActionButton fab_add;
+    RecyclerView contactsRecycler;
+    EditText barreRecherche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_contacts);
 
+        contactsRecycler=(RecyclerView) findViewById(R.id.list_contacts);
+        barreRecherche=(EditText) findViewById(R.id.search_text);
         fab_add=(FloatingActionButton) findViewById(R.id.fab_add);
         fab_add.setOnClickListener(this);
     }
@@ -30,3 +45,6 @@ public class Liste_contacts extends AppCompatActivity implements View.OnClickLis
         }
     }
 }
+
+
+
